ebd94d18676785be974399727d4426849a316081
Maximdwdaw <142977598+Maximdwdaw@users.noreply.github.com>
2023-11-07 13:57:57 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+console.log(12038675888888888888888888888888888888888)
+
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log('Server is running on port 3000');
+});
